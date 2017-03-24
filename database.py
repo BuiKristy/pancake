2453588def7ca0b5fba8a5f9795e4d4af2a86be2
Kristy Bui <bui.kristy@gmail.com>
2017-03-24 16:37:50 -0700

    Add TODOs for database

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('sqlite:///playlists.db', echo=True)
 Base = declarative_base()
 
 class Playlist(Base):
@@ -22,3 +22,23 @@ class Content(Base):
 
     def __repr__(self):
         return "<Content(playlist_id='%s', song_name='%s', order='%s')>" % (self.playlist_id, self.song_name, self.order)
+
+# TODO get_playlists()
+    # returns a map of playlist ids to playlist name 
+
+# TODO get_songs_from_playlist(playlist_id)
+    # returns a list of song names
+
+# TODO create_playlist(playlist_name)
+    # returns id of the playlist created
+
+# TODO delete_playlist(playlist_id)
+
+# TODO update_playlist_name(playlist_id, name)
+
+# TODO add_song_to_playlist(playlist_id, song_name)
+
+# TODO delete_song_from_playlist(index)
+
+# TODO reorder_songs_in_playlist(playlist_id, old_index, new_index)
+    # moves song at old index to the new index and shifts the rest of the songs down
