93d0b2fbf42a5471f0ae1dfe935ed04d5b7d3ba5
Kristy Bui <bui.kristy@gmail.com>
2017-03-26 15:05:57 -0700

    Add docstrings to all methods

--- database.py
+++ database.py
@@ -38,7 +38,7 @@ class PlaylistDB:
         Base.metadata.create_all(self.engine)
 
     def get_playlists(self):
-        # returns a map of playlist ids to playlist name
+        """ Returns a map of playlist ids to playlist name """
         session = self.Session()
         result = dict()
 
@@ -49,7 +49,7 @@ class PlaylistDB:
         return result
 
     def get_songs_from_playlist(self, playlist_id):
-        # returns a list of song names
+        """ Returns a list of song names in the playlist with the given playlist id """
         session = self.Session()
         result = session.query(Content).filter_by(playlist_id=playlist_id).\
             order_by(Content.order).all()
@@ -57,7 +57,7 @@ class PlaylistDB:
         return [row.song_name for row in result]
 
     def create_playlist(self, playlist_name):
-        # returns id of the playlist created
+        """ Creates and returns the id of the playlist using the given playlist name """
         session = self.Session()
         new_playlist = Playlist(name=playlist_name)
         session.add(new_playlist)
@@ -68,12 +68,14 @@ class PlaylistDB:
         return playlist_id
 
     def delete_playlist(self, playlist_id):
+        """ Deletes the playlist matching the given playlist id """
         session = self.Session()
         session.delete(session.query(Playlist).filter(Playlist.id==playlist_id).one())
         session.commit()
         session.close()
 
     def update_playlist_name(self, playlist_id, new_name):
+        """ Updates the playlist name given the playlist id and the new name """
         session = self.Session()
         session.query(Playlist).filter(Playlist.id==playlist_id).update({"name": new_name})
         
@@ -81,6 +83,7 @@ class PlaylistDB:
         session.close()
 
     def add_song_to_playlist(self, playlist_id, song_name):
+        """ Adds the given song name to the playlist matching the playlist id """
         session = self.Session()
         max_order = session.query(func.max(Content.order + 1)).filter_by(playlist_id=playlist_id).scalar()
         
@@ -98,6 +101,7 @@ class PlaylistDB:
             session.close()
 
     def delete_song_from_playlist(self, playlist_id, index):
+        """ Deletes the song at the given index from the playlist matching the given playlist id """
         session = self.Session()
         session.delete(session.query(Content).filter(Content.playlist_id==playlist_id, \
             Content.order==index).one())
@@ -107,7 +111,7 @@ class PlaylistDB:
         session.close()
 
     def reorder_songs_in_playlist(self, playlist_id, old_index, new_index):
-        # moves song at old index to the new index and shifts the rest of the songs down
+        """ Moves the song at the given old index to the new index in the playlist given the playlist id """
         session = self.Session()
         session.query(Content).filter(Content.order==old_index, Content.playlist_id==playlist_id).\
             update({"order": -1})
