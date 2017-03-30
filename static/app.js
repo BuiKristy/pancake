f645ff125a996acc0e8c064a245a361883d65650
Kristy Bui <bui.kristy@gmail.com>
2017-03-29 19:16:22 -0700

    Add playlist renaming

--- static/app.js
+++ static/app.js
@@ -23,6 +23,10 @@
     }
 })*/
 
+var store = {
+
+}
+
 var app = new Vue ({
     el: "#app",
     data: {
@@ -38,14 +42,23 @@ var app = new Vue ({
 
         localChangeSong: function(index, arr, start_index, end_index) {
             this.model.playlists[index].songs = arr;
-            console.log(this)
 
             $.ajax({
                 method: 'PATCH',
                 url: '/playlists/' + this.model.playlists[index].id + '/' + start_index,
                 data: {new_index: end_index}
+            }).done(this.refreshModel)
+        },
+
+        localChangeName: function(index, new_name) {
+            this.model.playlists[index].name = new_name;
+
+            $.ajax({
+                method: 'PATCH',
+                url: '/playlists/' + this.model.playlists[index].id,
+                data: {playlist_name: new_name}
             }).done(function() {
-                console.log("hi there");
+                console.log("renamed playlist");
             })
         }
     }
@@ -58,21 +71,33 @@ Vue.component('playlist-item', {
             return 'playlist' + this.playlistInfo.id
         }
     },
-    template: '<div><h3>{{ playlistInfo.name }}</h3> <sortable v-on:sortable-change="echo" v-bind:song-list="playlistInfo.songs"' +
-        'v-bind:index="index"></sortable></div>',
+    template: '<div><h3 v-if="!editing">{{ playlistInfo.name }}</h3> <input v-else v-bind:value="playlistInfo.name" v-on:input="namePlaylist"> <button v-on:click="editPlaylistName"></button> <sortable v-on:sortable-change="echo"' +
+        'v-bind:song-list="playlistInfo.songs"></sortable></div>',
     methods:  {
-        echo: function(index, arr, start_index, end_index) {
-            this.$emit('sortable-change', index, arr, start_index, end_index);
+        echo: function(arr, start_index, end_index) {
+            this.$emit('sortable-change', this.index, arr, start_index, end_index);
+        },
+
+        editPlaylistName: function() {
+            this.editing = !this.editing;
+        },
+
+        namePlaylist: function(new_name) {
+            this.$emit('edit-playlist', this.index, new_name.target.value)
+        }
+    },
+    data: function() {
+        return {
+            editing: false
         }
     }
 })
 
 Vue.component('sortable', {
-    props: ['songList', 'index'],
+    props: ['songList'],
     template: '<ul><li v-for="song in songList">{{ song }}</li></ul>',
     mounted: function() {
         var vm = this;
-        console.log(this.index);
         $(this.$el)
             .sortable({
                 axis: "y", 
@@ -88,7 +113,7 @@ Vue.component('sortable', {
                     end_index = ui.item.index();
                     $(vm.$el).sortable("cancel");
                     start_index = ui.item.index();
-                    vm.$emit('sortable-change', vm.index, x, start_index, end_index)
+                    vm.$emit('sortable-change', x, start_index, end_index)
                 }
             })
             .disableSelection();
